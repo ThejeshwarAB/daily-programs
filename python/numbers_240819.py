33c9c5a28be9f8e6f7ad015211d023f5fcbd0480
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-08-25 20:11:57 +0530

    Create numbers_240819.py

---
+++ python/numbers_240819.py
@@ -0,0 +1,16 @@
+#sourcecode
+n,x=map(int,input().split())
+while(x):
+    y=x*n
+    print(f"{x} * {n} = {y}")
+    x-=1
+
+Sample IP:
+4 5
+
+Sample OP:
+5 * 4 = 20
+4 * 4 = 16
+3 * 4 = 12
+2 * 4 = 8
+1 * 4 = 4
