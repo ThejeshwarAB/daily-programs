8d899d5f4f47a02ec7b390e918c3956d50a4ba20
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-09-11 19:36:53 +0530

    Create numbers_110919.c

---
+++ c/numbers_110919.c
@@ -0,0 +1,44 @@
+//sourcecode
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+int a,o,e,i,p,q;
+o=e=p=q=0;
+while(scanf("%d",&a)==1)
+{
+    if(a%2)
+    {
+        o+=a;
+        p++;
+    }
+    else if(a!=0)
+    {
+        e+=a;
+        q++;
+    }
+}
+if(q)
+printf("%d",e);
+else
+printf("-1");
+if(p)
+printf("\n%.1f",(float)o/p);
+else
+printf("\n-1");
+}
+//find number of odd
+//find number of evn
+//print even sum
+//print odd aveg
+//else print -1
+//neglect zero
+
+/* 
+Sample IP:
+1 2 3 4
+
+Sample OP:
+6     (2+4)
+2.0   ((1+3)/2)
+*/
