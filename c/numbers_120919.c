a87fab08a9e5305df40473f38eac1d59f2c0dd01
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-09-12 20:26:47 +0530

    Create numbers_120919.c

---
+++ c/numbers_120919.c
@@ -0,0 +1,20 @@
+//sourcecode
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+int n;
+scanf("%d",&n);
+printf("%c",n);
+}
+//find ASCII eq
+//print as char
+
+/*
+Sample IP:
+48
+
+Sample OP:
+0
+(Character 0)
+*/
