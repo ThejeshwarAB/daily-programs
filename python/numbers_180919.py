fea0db1d16736a2ebd4bd4dbf06ca093a703e485
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-09-18 22:37:00 +0530

    Create numbers_180919.py

---
+++ python/numbers_180919.py
@@ -0,0 +1,17 @@
+#sourcecode
+n=input()
+x=n.replace("a","1")
+y=x.replace("b","0")
+print(int(y,2))
+#replace a with 1
+#replace b with 0
+#print bin as dec
+
+'''
+Sample IP:
+aab
+
+Sample OP:
+6
+(110b to dec)
+'''
