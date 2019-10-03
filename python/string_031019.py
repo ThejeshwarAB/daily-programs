6ca4028e67158ff722cc09976921bfaa98191d36
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-10-03 22:06:48 +0530

    Create string_031019.py

---
+++ python/string_031019.py
@@ -0,0 +1,16 @@
+s=input()
+x=int(input())
+s=s[x-1::-1]
+print(s.swapcase())
+#swapcase of string
+#print first x char
+#reverse is printed
+
+'''
+Sample IP:
+abcDEfghi
+5
+
+Sample OP:
+edCBA
+'''
