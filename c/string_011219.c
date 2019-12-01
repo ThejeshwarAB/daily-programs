70283e32d80174b7301e86b4b1eebfd4dea9d986
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-12-01 22:16:46 +0530

    Create string_011219.c

---
+++ c/string_011219.c
@@ -0,0 +1,19 @@
+//sourcecode
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+char c1,c2;
+scanf("%c %c",&c1,&c2);
+for(int i=c1;i<=c2;i++)
+printf("%d ",i);
+}
+//print ASCII in range
+
+/*
+Sample IP:
+a e
+
+Sample OP:
+98 99 100 101 102
+*/
