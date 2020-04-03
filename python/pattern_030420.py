030533538ece3ea6920510c720954b6441bb8f26
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2020-04-03 22:44:31 +0530

    Create pattern_030420.py

---
+++ python/pattern_030420.py
@@ -0,0 +1,28 @@
+#sourcecode
+n=int(input())
+x=[]
+for i in range(n):
+    x.append(input().split(' '))
+t=input()
+for i in x:
+    c,k=0,0
+    for j in i:
+        if(j[0].lower()==t[k].lower()):
+            c+=1
+            k+=1
+        else: break
+    if(c==len(t)):
+        print(*i)
+#input n no. of strings
+#print the abbreviation
+
+'''
+Sample IP:
+2
+world wide web
+world health org
+WHO
+
+Sample OP:
+word health org
+'''
