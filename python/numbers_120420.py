01fe0dc51a6b516eb69a47a52d15338067290aa7
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2020-04-12 22:23:49 +0530

    Create numbers_120420.py

---
+++ python/numbers_120420.py
@@ -0,0 +1,16 @@
+#sourcecode
+d={1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
+n=int(input())
+if n not in d:
+    print("Invalid")
+else:
+    print(d[n])
+#print no. of days in non leap year
+
+'''
+Sample IP:
+1
+
+Sample OP:
+31
+'''
